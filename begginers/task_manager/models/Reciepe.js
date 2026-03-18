340dffe8ebc26f77aa390ab3fddff82b5093b28d
Shibajyoti Maity <shibajyoti.maity06@gmail.com>
2026-03-18 23:41:33 +0530

    add Reciepe model with schema definition for title, description, ingredients, instructions, cooking time, servings, and createdBy

--- begginers/task_manager/models/Reciepe.js
+++ begginers/task_manager/models/Reciepe.js
@@ -1,5 +1,49 @@
 const mongoose = require('mongoose')
 
 const Reciepe = new mongoose.Schema({
-    
-})
+	title: {
+		type: String,
+		required: true,
+		trim: true,
+		minLength: 3
+	},
+	description: {
+		type: String,
+		trim: true,
+		default: ''
+	},
+	ingredients: {
+		type: [String],
+		required: true,
+		validate: {
+			validator: function (value) {
+				return Array.isArray(value) && value.length > 0
+			},
+			message: 'At least one ingredient is required'
+		}
+	},
+	instructions: {
+		type: String,
+		required: true,
+		trim: true,
+		minLength: 10
+	},
+	cookingTime: {
+		type: Number,
+		required: true,
+		min: 1
+	},
+	servings: {
+		type: Number,
+		default: 1,
+		min: 1
+	},
+	createdBy: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
+}, { timestamps: true })
+
+const reciepeModel = mongoose.model('Reciepe', Reciepe)
+
+module.exports = { reciepeModel }
